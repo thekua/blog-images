fa5f2569d941e5f375a964ed12c0f50c09ecee79
Patrick Kua <pkua@thoughtworks.com>
2012-04-21 12:08:23 +0100

    Make it use the test file

--- spec/PhotoSpec.rb
+++ spec/PhotoSpec.rb
@@ -1,7 +1,9 @@
 require 'rspec'
+require File.dirname(__FILE__) + '/../src/Photo.rb'
+# TODO: Must work out better way of doing this
 
 describe 'Photo' do
    it "should " do
-     "s".should eq "s"
+     Photo.new(File.dirname(__FILE__) + '/MuffinsSmall.jpg')
    end
 end
